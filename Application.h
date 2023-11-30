af69545102095088449de7ba6b1edd9599c2ec8f
MFRSiam <mfrforhad1@gmail.com>
2023-12-01 05:08:46 +0600

    Removed All

--- Application.h
+++ Application.h
@@ -9,57 +9,6 @@
 #include <math.h>
 #include <string.h>
 
-// Every Actor in the Scene is a CELL
-enum CellType{
-    Wood,
-    Sand,
-    Water,
-    Floor,
-    Null
-}typedef CellType;
 
-
-struct Cell{
-    Vector2 Pos;
-    CellType Type;
-    float velocity;
-}typedef Cell;
-
-
-
-
-/*! @brief This Crates a Floor in the Array consisting of f as a indicator of being floor.
- * @param ptr The Cells that will be modified
- * @param x The width of the play area
- * @param y The height of the play area
- */
-void SetFloor(Cell **ptr,int x,int y);
-
-void SpawnSandAtMouseLocation(Cell **ptr,Vector2 *mousePos,float resulution);
-void SpawnWoodAtMouseLocation(Cell **ptr,Vector2 *mousePos,float resulution);
-void SpawnWaterAtMouseLocation(Cell **ptr,Vector2 *mousePos, float resulution);
-
-/*! @brief Creates a Debug file in the dir named dump.txt for some dumps
- * @param ptr The Array data which will be dumped
- * @param playHeight Play Area Y
- * @param playWidth Play Area X
- */
-void DumpFile(Cell **ptr,int playHeight,int playWidth);
-
-
-void ApplicationDraw(Cell **ptr,Vector2 size,int playHeight,int playWidth);
-
-
-void UpdateMap(Cell **ptr,int height_y,int width_x);
-
-void InitiateCells(Cell **ptr,int playHeight,int playWidth,int screenHeight, float resulotion);
-void ResetMap(Cell **ptr,int playHeight,int playWidth);
-
-void checkMode(char *mode);
-
-void SpawnStuff(Cell **ptr,Vector2 *mousePos,const char *mode,int screenWidth,int screenHeight, float resulution);
-
-void SandLogic(Cell **ptr,int i, int j,int height_y,int width_x);
-void WaterLogic(Cell **ptr,int i, int j,int height_y,int width_x);
 
 #endif //SANDSIMULATION_RAYLIB_APPLICATION_H
--- main.c
+++ main.c
@@ -5,59 +5,23 @@
 
 
 
-int main() {
-//    const int screenWidth = 800;
-//    const int screenHeight = 450;
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    const int screenWidth = 1000;
-    const int screenHeight = 800;
-    const float resolution = 8;
-    int playHeight = (int)((float)screenHeight / resolution);
-    int playWidth = (int)((float)screenWidth / resolution);
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    SetTargetFPS(60);
 
-    InitWindow(screenWidth, screenHeight, "My Basic Window");
-    SetTargetFPS(144);
-
-    Cell** cells = (Cell**) calloc(playHeight,sizeof(Cell**));
-    for(int i=0;i<playHeight;i++){
-        cells[i] = (Cell*) calloc(playWidth,sizeof(Cell));
-    }
-
-    InitiateCells(cells, playHeight, playWidth, screenHeight, resolution);
-    SetFloor(cells,playWidth,playHeight);
-
-    Vector2 mousePosition;
-    Vector2 size = {resolution, resolution};
-    char mode = 's';
     while (!WindowShouldClose())
     {
-
-        mousePosition = GetMousePosition();
-        checkMode(&mode);
-        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
-
-            SpawnStuff(cells, &mousePosition, &mode, screenWidth, screenHeight, resolution);
-        }
-        else if (IsKeyDown(KEY_R)){
-            ResetMap(cells,playHeight,playWidth);
-        }
-
-        double time = GetTime();
-
-        //printf("%f ,",time);
-        if(time>1.0f){
-            UpdateMap(cells,playHeight,playWidth);
-            time = 0.0f;
-        }
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        ApplicationDraw(cells,size,playHeight,playWidth);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
-
     }
-    DumpFile(cells,playHeight,playWidth);
     CloseWindow();
-    free(cells);
-    return EXIT_SUCCESS;
+
+    return 0;
 }
 
